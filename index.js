1be3fa503578f70400ed1e61a1ced5c198b12c5e
guistetter <guilherme_stetter@hotmail.com>
2020-08-24 15:04:18 -0300

    configuracoes inicias servidor e mongoose

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express")
 const app = express()
-const port = 3000
-const mongoose = require('mongoose')
+const port = process.env.PORT || 3000
+
+const mongoose = process.env.MONGO || require('mongoose')
+mongoose.Promise = global.Promise
+const mongo = "mongodb://localhost/minhaserie-rest-devpleno"
 
 const series = require('./routes/series')
 
 app.use('/series',series)
 //app.get('/series', (req, res) => res.send(series))
-
-app.listen(port, () => console.log("app listening on port: ",port))
+mongoose.connect(mongo, {useMongoClient: true})
+.then(()=> {
+  app.listen(port, () => console.log("app listening on port: ",port))
+})
+.catch(e => console.log(e))
