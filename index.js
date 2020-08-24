681e0d4ff55b4a7ea394e70ebc14ec9e305fe3be
guistetter <guilherme_stetter@hotmail.com>
2020-08-24 16:02:13 -0300

    criando rota para autenticacao

--- index.js
+++ index.js
@@ -7,14 +7,52 @@ const mongoose = process.env.MONGO || require('mongoose')
 mongoose.Promise = global.Promise
 const mongo = "mongodb://localhost/minhaserie-rest-devpleno"
 
-app.use(bodyParser({extended: true}))
+//app.use(bodyParser({extended: true}))
+app.use(bodyParser.json())
 
 const series = require('./routes/series')
+const User = require("./models/user")
 
 app.use('/series',series)
 //app.get('/series', (req, res) => res.send(series))
+
+app.post('/auth', async(req, res) =>{
+  const user = req.body
+  const userDb = await User.findOne({username: user.username})
+  if(userDb){
+    if(userDb.password === user.password){
+      res.send('ok autentico e entrou')
+    }else{
+      res.send('wrong credentials')
+    }
+  }else{
+    res.send('wrong credentials')
+  }
+  //res.send(user)
+})
+
+const createInitialUsers = async() => {
+  const total = await User.count({})
+  if(total === 0){
+    const user = new User({
+      username: 'gui',
+      password: '123456',
+      roles:['restrito', 'admin']
+    })
+    await user.save()
+
+    const user2 = new User({
+      username: 'restrito',
+      password: '123456',
+      roles:['restrito']
+    })
+    await user2.save()
+  }
+}
+
 mongoose.connect(mongo, {useMongoClient: true})
 .then(()=> {
+  createInitialUsers()
   app.listen(port, () => console.log("app listening on port: ",port))
 })
 .catch(e => console.log(e))
