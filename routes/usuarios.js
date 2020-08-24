99a835ed7f480def9792423666ae41bdc94fb2a0
guistetter <guilherme_stetter@hotmail.com>
2020-08-24 17:23:47 -0300

    criando rotas autenticadas para usuarios, só adm pode visualizar os usuarios

---
+++ routes/usuarios.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
+const jwtSecret = "abc123abc"
+const User = require('../models/user')
+
+//midleware autorizacao para navegar em series
+router.use(async(req,res, next) =>{
+  const token = req.headers['x-access-token'] || req.body.token || req.query.token
+  if(token){
+    try{
+      const payload = jwt.verify(token, jwtSecret)
+      if(payload.roles.indexOf('admin')>= 0){
+        next()
+      } else{
+        res.send({success:false})
+      }
+    }catch(e){
+      res.send({success:false})
+    }
+  } else {
+    res.send({success:false})
+  }
+})
+
+router.get('/', async(req,res) => {
+  const users = await User.find({})
+  res.send(users)
+})
+
+
+module.exports = router
