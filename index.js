b2295da009ab077f87c36ba0bdc65c7159eaa649
guistetter <guilherme_stetter@hotmail.com>
2020-08-24 15:13:58 -0300

    configurando body-parser e middleware para ter acesso ao req.body

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
+const bodyParser = require("body-parser")
 
 const mongoose = process.env.MONGO || require('mongoose')
 mongoose.Promise = global.Promise
 const mongo = "mongodb://localhost/minhaserie-rest-devpleno"
+
+app.use(bodyParser({extended: true}))
 
 const series = require('./routes/series')
 
